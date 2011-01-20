b238fcdedeb01c564e477a6b32168b9eb2146d98
Ilja Krijger <ilja@quittheprogram.org>
2011-01-20 15:56:57 +0100

    add listview

--- src/org/quittheprogram/benzin/Fillings.java
+++ src/org/quittheprogram/benzin/Fillings.java
@@ -1,12 +1,18 @@
 package org.quittheprogram.benzin;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.*;
 
-public class Fillings extends Activity  {
-	@Override
-	protected void onCreate(Bundle savedInstanceState){
+public class Fillings extends ListActivity {	
+	private String lv_arr[]={"filling1","filling2","filling3","filling4", "filling5", "filling6", "filling7", "filling8", "filling9"};
+	
+	@Override	
+	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fillings);
+		
+		setListAdapter(new ArrayAdapter<String>(this,
+		          android.R.layout.simple_list_item_1, lv_arr));
+		  getListView().setTextFilterEnabled(true);		
 	}
 }
