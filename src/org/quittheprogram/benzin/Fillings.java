136de1814bb6b03229a724b30016418d758e8ef5
Ilja Krijger <ilja@quittheprogram.org>
2011-01-23 12:44:46 +0100

    add context menu to fillings list

--- src/org/quittheprogram/benzin/Fillings.java
+++ src/org/quittheprogram/benzin/Fillings.java
@@ -1,21 +1,34 @@
 package org.quittheprogram.benzin;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.*;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
 
 public class Fillings extends ListActivity {	
-	private String lv_arr[]={"filling1","filling2","filling3","filling4", "filling5", "filling6", "filling7", "filling8", "filling9"};
-	
+		
 	@Override	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		
+		// register all items in list for a context menu
+		registerForContextMenu(getListView());
+		//getListView().setOnCreateContextMenuListener(this);
 		
 		DatabaseHelper dbHelper = new DatabaseHelper(this);
 	    Cursor cursor = dbHelper.getFillings();
         startManagingCursor(cursor);
         
+       
         
        
 		
@@ -23,5 +36,27 @@ public class Fillings extends ListActivity {
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.filling, cursor, dbHelper.FROM, dbHelper.TO);
         setListAdapter(adapter);
 		getListView().setTextFilterEnabled(true);		
+	}
+	
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.filling_context_menu, menu);
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item){
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		
+		switch (item.getItemId()){
+		case R.id.edit_filling:
+			startActivity(new Intent(this, AddFilling.class));
+			return true;
+		default: 
+			return super.onContextItemSelected(item);
+		}
+		
 	}
 }
